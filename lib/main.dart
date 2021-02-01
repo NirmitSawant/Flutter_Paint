b87f4056ef6c6fb9e8ce8691f3cf85a785fe88bd
NirmitSawant <sawantnirmit@gmail.com>
2021-02-01 15:20:11 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,226 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Paint',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class DrawingArea {
+  Offset point;
+  Paint areaPaint;
+
+  DrawingArea({this.areaPaint, this.point});
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<DrawingArea> points = [];
+  Color selectedColor;
+  double strokewidth;
+
+  void selectColor() {
+    showDialog(
+      context: context,
+      // ignore: deprecated_member_use
+      child: AlertDialog(
+        title: Text("Choose Color"),
+        content: SingleChildScrollView(
+          child: BlockPicker(
+            pickerColor: selectedColor,
+            onColorChanged: (color) {
+              setState(() {
+                selectedColor = color;
+              });
+            },
+          ),
+        ),
+        actions: [
+          FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("close"))
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    selectedColor = Colors.black;
+    strokewidth = 2.0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                  Color.fromRGBO(138, 35, 135, 1.0),
+                  Color.fromRGBO(233, 64, 87, 1.0),
+                  Color.fromRGBO(242, 113, 33, 1.0),
+                ])),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: width * 0.80,
+                  height: height * 0.80,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.4),
+                          blurRadius: 5.0,
+                          spreadRadius: 1.0,
+                        ),
+                      ]),
+                  child: GestureDetector(
+                    onPanDown: (details) {
+                      this.setState(() {
+                        points.add(DrawingArea(
+                            point: details.localPosition,
+                            areaPaint: Paint()
+                              ..strokeCap = StrokeCap.round
+                              ..isAntiAlias = true
+                              ..color = selectedColor
+                              ..strokeWidth = strokewidth));
+                      });
+                    },
+                    onPanUpdate: (details) {
+                      this.setState(() {
+                        points.add(DrawingArea(
+                            point: details.localPosition,
+                            areaPaint: Paint()
+                              ..strokeCap = StrokeCap.round
+                              ..isAntiAlias = true
+                              ..color = selectedColor
+                              ..strokeWidth = strokewidth));
+                      });
+                    },
+                    onPanEnd: (details) {
+                      this.setState(() {
+                        points.add(null);
+                      });
+                    },
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      child: CustomPaint(
+                        painter: MyCustomPainter(points: points),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                  width: width * 0.80,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      IconButton(
+                          icon: Icon(
+                            Icons.color_lens_sharp,
+                            color: selectedColor,
+                          ),
+                          onPressed: () {
+                            selectColor();
+                          }),
+                      Expanded(
+                          child: Slider(
+                        value: strokewidth,
+                        onChanged: (value) {
+                          setState(() {
+                            strokewidth = value;
+                          });
+                        },
+                        min: 1.0,
+                        max: 7.0,
+                        activeColor: selectedColor,
+                      )),
+                      IconButton(
+                          icon: Icon(Icons.layers_clear_sharp),
+                          onPressed: () {
+                            setState(() {
+                              points.clear();
+                            });
+                          }),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyCustomPainter extends CustomPainter {
+  List<DrawingArea> points;
+  Color color;
+  double stokewidth;
+
+  MyCustomPainter({this.points});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint background = Paint()..color = Colors.white;
+    Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
+
+    canvas.drawRect(rect, background);
+
+    for (int x = 0; x < points.length - 1; x++) {
+      if (points[x] != null && points[x + 1] != null) {
+        Paint paint = points[x].areaPaint;
+        canvas.drawLine(points[x].point, points[x + 1].point, paint);
+      } else if (points[x] != null && points[x + 1] == null) {
+        Paint paint = points[x].areaPaint;
+        canvas.drawPoints(PointMode.points, [points[x].point], paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+}
